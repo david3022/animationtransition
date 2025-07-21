a5855557e50672587f69f02a3579bc47e741125e
David3022 <davidafs36@gmail.com>
2025-07-21 11:29:52 -0600

    changed carrousel

---
+++ astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+// import { defineConfig } from 'astro/config';
+// import mdx from '@astrojs/mdx';
+// import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+// export default defineConfig({
+// 	site: 'https://example.com',
+// 	integrations: [mdx(), sitemap()],
+// });
+
+
+import { defineConfig } from 'astro/config';
+
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from '@astrojs/tailwind'; // 👈 agrega esta línea
+
+export default defineConfig({
+    site: 'https://example.com',
+    integrations: [mdx(), sitemap(), tailwind()], // 👈 agrégalo aquí también
+});
